63b52ae4233cad4e8cc711a016721395eaeefee7
hiromaite <x.raymcd@gmail.com>
2021-11-11 15:14:03 +0900

    [update] algorithm, output function

--- ICC_calculator.py
+++ ICC_calculator.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 flg_rot = False  # flag to enable rotation function
 flg_test = False  # for review
 flg_figure_out = True  # for review
+flg_picture_out = True  # for review
 
 # For religious reasons, the direction of the tick marks on the diagram should be inward.
 plt.rcParams['xtick.direction'] = 'in'
@@ -96,7 +97,7 @@ def rot_img(img):
 
 # Constants for m-mode slicer
 cs_center = 150  # center of cross-section
-mean_width = 16  # diameter of ultra-sound wave beam
+mean_width = 32  # diameter of ultra-sound wave beam
 
 # Open file select dialog, check file exist
 root = tkinter.Tk()
@@ -120,12 +121,12 @@ if len(files) % 2 != 0:
 out = [[] for i in range(len(files) // 2)]
 
 # iteration per file
-for counter, file in enumerate(tqdm(files)):
+for counter, file in enumerate(tqdm(files, leave=False)):
     # get file data, param.
     path = os.path.dirname(file)
     filename = os.path.splitext(os.path.basename(file))[0]
     img = cv2.imread(file)  # image object
-    img_draw = cv2.imread(file)  # image object drawing use
+    img_draw = img.copy()  # image object drawing use
 
     #  Set variables about video object
     if len(img.shape) == 3:
@@ -162,7 +163,11 @@ for counter, file in enumerate(tqdm(files)):
 
     # origin correction and normalization based on min and max
     max_conv_data = max(conv_data[15:-15])
-    min_conv_data = min(conv_data[:165])
+    if min_conv_data > 5:
+        id_last_local_min = max(np.squeeze(signal.argrelmin(conv_data[:165])))
+        min_conv_data = conv_data[id_last_local_min]
+    else:
+        min_conv_data = min(conv_data[:165])
     conv_data_norm = ((conv_data - min_conv_data)
                       / (max_conv_data - min_conv_data))
 
@@ -175,16 +180,20 @@ for counter, file in enumerate(tqdm(files)):
                            & (conv_data_norm[: -1] < 0.1), 0, 0.05)
 
     # get the integral value for each sound area
-    int_int_data = silent_area  # var for interval integral
+    int_int_data = silent_area.copy()  # var for interval integral
     for i in range(len(int_int_data) - 1):
         if silent_area[i + 1]:  # 0 means False
             int_int_data[i + 1] = int_int_data[i] + silent_area[i + 1]
-        mass_dist_data = int_int_data
+
+    # give the integral value to the sound area
+    mass_dist_data = int_int_data
     for i in range(len(mass_dist_data) - 1)[::-1]:
         if int_int_data[i - 1] and int_int_data[i - 1] < int_int_data[i]:
             mass_dist_data[i - 1] = int_int_data[i]
     mass_dist_data[-1] = 0
     mass_dist_data_all = mass_dist_data.copy()
+
+    # normalized by max intensity after removing the area of multiple intensities
     mass_dist_data_top = np.zeros(len(mass_dist_data), np.float16)
     for i in range(len(mass_dist_data)):
         if not(mass_dist_data[i]):
@@ -194,7 +203,7 @@ for counter, file in enumerate(tqdm(files)):
     mass_dist_data = mass_dist_data / (10 * max_mass_data)
     mass_dist_data_all = mass_dist_data_all / (10 * max_mass_data)
 
-    # find the highest and second highest masses(=peak group)
+    # find the highest and second highest masses(=peak group or sound area)
     mass_first = -1
     mass_second = -1
     for i in range(len(mass_dist_data)):
@@ -252,10 +261,10 @@ for counter, file in enumerate(tqdm(files)):
     # append data into output
     pair = counter // 2
     if counter % 2 == 0:  # Processing of even-numbered files
-        out[pair] = [filename, depth_bottom, depth_bottom - depth_top]
+        out[pair] = [filename, depth_bottom, depth_top]
     else:
-        out[pair].extend([filename, depth_bottom, depth_bottom - depth_top,
-                         out[pair][1] - depth_bottom, out[pair][2] - (depth_bottom - depth_top)])
+        out[pair].extend([filename, depth_bottom, depth_top, out[pair][1] - depth_bottom,
+                         (out[pair][1] - out[pair][2]) - (depth_bottom - depth_top)])
 
     # test output
     if flg_test:
@@ -270,20 +279,27 @@ for counter, file in enumerate(tqdm(files)):
     if flg_figure_out:
         x = np.arange(0, 17, 17/300)
         fig_out, axes = plt.subplots(
-            nrows=2, ncols=1, squeeze=False, sharex='all', figsize=(6.4, 4.8))  # default dpi = 180
+            nrows=3, ncols=1, squeeze=False, sharex='all', figsize=(12.8, 14.4))  # default dpi = 180
 
-        axes[0, 0].plot(x, one_dim_data)
-        axes[0, 0].plot(x, conv_data)
+        axes[0, 0].plot(x, one_dim_data, label='one dim. data')
+        axes[0, 0].plot(x, conv_data, label='convolved data')
         axes[0, 0].set_xlim(0, 17)
         axes[0, 0].set_ylim(0, 180)
         axes[0, 0].set_ylabel("Intensity")
 
-        axes[1, 0].plot(x[: -1], diff_data)
-        axes[1, 0].plot(x[: -1], mass_dist_data)
-        axes[1, 0].plot(x[: -1], mass_dist_data_top)
-        axes[1, 0].set_ylim(-0.1, 0.1)
-        axes[1, 0].set_xlabel("Depth")
-        axes[1, 0].set_ylabel("Difference")
+        axes[1, 0].plot(x, conv_data_norm, label='normalized conv. data')
+        axes[1, 0].set_ylim(0, 1.2)
+        axes[1, 0].set_ylabel("Normalized intensity")
+
+        axes[2, 0].plot(x[: -1], diff_data, label='diffed data')
+        axes[2, 0].plot(x[: -1], mass_dist_data, label='bladder bottom')
+        axes[2, 0].plot(x[: -1], mass_dist_data_top, label='bladder top')
+        # axes[1, 0].plot(x[: -1], mass_dist_data_all, label='all mass distributions')
+        axes[2, 0].plot(x[: -1], silent_area, label='silent area')
+        axes[2, 0].set_ylim(-0.1, 0.1)
+        axes[2, 0].set_xlabel("Depth")
+        axes[2, 0].set_ylabel("Difference")
+        plt.legend()
 
         fig_out.align_labels(axes)
 
@@ -291,6 +307,15 @@ for counter, file in enumerate(tqdm(files)):
         fig_out.savefig(path_figure)
         plt.clf
         plt.close()
+
+    # output processing results as a echo image
+    if flg_picture_out:
+        for i in range(mean_width):
+            # calc. average in each rows(= num of mean_width)
+            img[id_top, int(cs_center - (mean_width / 2) + i), 2] = 255
+            img[id_bottom, int(cs_center - (mean_width / 2) + i), 2] = 255
+        path_picture = os.path.join(path + '/picture', filename + '.jpg')
+        cv2.imwrite(path_picture, img)
 
 # test output
 if flg_test:
