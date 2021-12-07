759b272418b0ed26536edf1e3e8f4ce722ad2bb1
Hiromasa Ito <x.raymcd@gmail.com>
2021-12-07 19:27:53 +0900

    [add] low pass filter

--- echo_data_analysis.py
+++ echo_data_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import signal
 from scipy.stats import norm
 
+
 def gaussian_filter(data, samplerate, freq):
     # get the convolution integral with the Gaussian function
     x_gauss = np.arange(-10, 10, 6 * freq / samplerate)
@@ -20,3 +21,29 @@ def highpass_filter(data, samplerate, freq_pass, freq_stop):
     b, a = signal.butter(n, Wn, "high")
     out = signal.filtfilt(b, a, data)
     return out
+
+
+def lowpass_filter(data, samplerate, freq_pass, freq_stop):
+    freq_n = samplerate / 2  # nyquist frequency
+    wp = freq_pass / freq_n
+    ws = freq_stop / freq_n
+    gain_pass = 3
+    gain_stop = 40
+    n, Wn = signal.buttord(wp, ws, gain_pass, gain_stop)
+    b, a = signal.butter(n, Wn, "low")
+    out = signal.filtfilt(b, a, data)
+    return out
+
+ 
+def attenuation_coefficient(data, attn_order):
+    # get attenuation coefficient from integrated values
+    size_data = len(data)
+    int_data = np.zeros(size_data, float)
+    int_data[0] = data[0]
+    for i in range(size_data - 1):
+        int_data[i + 1] = int_data[i] + data[i + 1]
+    int_data = int_data / int_data[-1]
+    out = int_data ** attn_order
+    return out
+
+
